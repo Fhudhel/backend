256a0f8a1d6bdf50aa34cb23122551a9002698a3
Fhudhel <111389711+Fhudhel@users.noreply.github.com>
2024-10-19 23:02:19 +0700

    Update rsvp.js

--- api/rsvp.js
+++ api/rsvp.js
@@ -3,18 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Koneksi ke MongoDB
 const mongoURI = "mongodb+srv://server123:Alfiahibnumalik@tesserver.rbuzv.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
-// Model untuk RSVP
 const rsvpSchema = new mongoose.Schema({
     name: String,
     attendance: Number,
@@ -23,7 +21,6 @@ const rsvpSchema = new mongoose.Schema({
 
 const Rsvp = mongoose.model('Rsvp', rsvpSchema);
 
-// Route untuk menghandle RSVP
 app.post('/api/rsvp', async (req, res) => {
     const { name, attendance, message } = req.body;
     const newRsvp = new Rsvp({ name, attendance, message });
@@ -36,7 +33,6 @@ app.post('/api/rsvp', async (req, res) => {
     }
 });
 
-// Route untuk mendapatkan semua RSVP
 app.get('/api/rsvps', async (req, res) => {
     try {
         const rsvps = await Rsvp.find();
@@ -46,5 +42,6 @@ app.get('/api/rsvps', async (req, res) => {
     }
 });
 
-// Export sebagai handler untuk Vercel
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
